b72f6ca3799122bcfcb1847465e901038dbe8b53
openapphub <openapphub@outlook.com>
2024-10-18 17:56:50 +0800

    Create PaddingLabel.h

---
+++ PaddingLabel.h
@@ -0,0 +1,13 @@
+//
+//  UIPaddingLabel.h
+//
+//  Created by openapphub on 2024/10/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaddingLabel : UILabel
+
+@property (nonatomic, assign) UIEdgeInsets textInsets;
+
+@end
